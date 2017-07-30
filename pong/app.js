f2dfca3febf7eec04731825cd89641232f395a3c
Kenn Costales <kennyfrc@Kenns-MacBook-Pro.local>
2017-07-30 14:28:17 +0800

    addded reset collision and ai

--- pong/app.js
+++ pong/app.js
@@ -5,6 +5,12 @@ var ballSpeedX = 10;
 var ballY = 30;
 var ballSpeedY = 4;
 var paddle1Y = 250;
+var paddle2Y = 250;
+var playerScore = 0;
+var computerScore = 0;
+const initialBallSpeedX = 10;
+const initialBallSpeedY = 4;
+const PADDLE_WIDTH = 10;
 const PADDLE_HEIGHT = 100;
 
 window.onload = function() {
@@ -20,11 +26,6 @@ window.onload = function() {
     });
 }
 
-function moveAndDrawEverything() {
-  moveEverything();
-  drawEverything();
-}
-
 function calculateMousePos(evt) {
 	var rect = canvas.getBoundingClientRect();
 	var root = document.documentElement;
@@ -36,15 +37,53 @@ function calculateMousePos(evt) {
 	};
 }
 
+function ballReset(){
+  ballX = canvas.width/2;
+  ballY = canvas.height/2;
+  ballSpeedX = -initialBallSpeedX
+  ballSpeedY = -initialBallSpeedY
+}
+
+function moveAndDrawEverything() {
+  moveEverything();
+  drawEverything();
+}
+
+function computerMoves() {
+  if (ballY > paddle2Y+PADDLE_HEIGHT/2) {
+    paddle2Y += 4;
+  } else {
+    paddle2Y -= 4;
+  }
+}
+
 function moveEverything() {
+  computerMoves();
+
   ballX = ballX + ballSpeedX;
   ballY = ballY + ballSpeedY;
 
+
   if (ballX < 0) {
-    ballSpeedX = -ballSpeedX;
+    if (ballY > paddle1Y && ballY < paddle1Y+PADDLE_HEIGHT) {
+      ballSpeedX = -ballSpeedX;
+      var deltaY = ballY - (paddle1Y+PADDLE_HEIGHT/2)
+      ballSpeedY = deltaY * 0.35
+    } else {
+      computerScore++;
+      ballReset();
+    }
   }
+
   if (ballX > canvas.width) {
-    ballSpeedX = -ballSpeedX;
+    if (ballY > paddle2Y && ballY < paddle2Y+PADDLE_HEIGHT) {
+      ballSpeedX = -ballSpeedX;
+      var deltaY = ballY - (paddle2Y+PADDLE_HEIGHT/2)
+      ballSpeedY = deltaY * 0.35
+    } else {
+      playerScore++;
+      ballReset();
+    }
   }
 
   if (ballY < 0) {
@@ -61,11 +100,16 @@ function drawEverything() {
   // ball
   colorBall(ballX,ballY,10,'white');
   // left paddle
-  colorRect(0,paddle1Y,10,PADDLE_HEIGHT,'white');
+  colorRect(0,paddle1Y,PADDLE_WIDTH,PADDLE_HEIGHT,'white');
   // right paddle
-  colorRect(790,PADDLE_HEIGHT,10,PADDLE_HEIGHT,'white');
+  colorRect(790,paddle2Y,PADDLE_WIDTH,PADDLE_HEIGHT,'white');
+  fillScore();
 }
 
+function fillScore(){
+  canvasContext.fillText(playerScore, 100,100);
+  canvasContext.fillText(computerScore, canvas.width-100,100);
+}
 function colorBall(centerX, centerY, radius, color) {
   canvasContext.fillStyle = color;
   canvasContext.beginPath();
